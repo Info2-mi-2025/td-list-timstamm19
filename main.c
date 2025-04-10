0b540a23f0a00e6f3b7ac38dc9e6f64a32c4d276
tim stamm <tim.stamm@heig-vd.ch>
2025-04-10 22:58:10 +0200

    7 test réussis

--- main.c
+++ main.c
@@ -3,11 +3,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_DATA 100
+
 // Ne pas modifier
 void init_file()
 {
     // data.txt
     FILE* f = fopen("data.txt", "w");
+    if (f == NULL) exit(2);
     if (f)
     {
         fprintf(f, "5\n12\n8\n1\n19\n");
@@ -53,8 +56,19 @@ typedef struct
 } List;
 
 // Fonctions de base
+bool is_number(const char* s) {
+    for (int i = 0; s[i]; i++) {
+        if (s[i] < '0' || s[i] > '9') return false;
+    }
+    return true;
+}
+
 void append(List* list, int value)
 {
+}
+
+void version(){
+    printf("version 1.0");
 }
 
 void free_list(List* list)
@@ -63,6 +77,14 @@ void free_list(List* list)
 
 void print_list(const List* list)
 {
+    Node* temp = list->head;
+    printf("Liste : ");
+    while (temp) {
+        printf("%d", temp->value);
+        if (temp->next) printf(" -> ");
+        temp = temp->next;
+    }
+    printf("\n");
 }
 
 void reverse_list(List* list)
@@ -71,17 +93,17 @@ void reverse_list(List* list)
 
 int sum_list(const List* list)
 {
-    return 0;
+
 }
 
 int min_list(const List* list)
 {
-    return 0;
+    
 }
 
 int max_list(const List* list)
 {
-    return 0;
+    
 }
 
 void filter_list(List* list, int threshold)
@@ -129,8 +151,88 @@ int main(int argc, char* argv[])
     // Ne pas modifier
     init_file();
     // ---------------
+    char* filename = NULL;
+    bool is_file = false;
+    bool option = false;
+    List liste = {0};
 
-    if(argc < 2) return 1;
-    
-    return 0;
+    if (argc < 2){
+        printf("Erreur pas assez d'argument");
+        return 1;
+    }
+
+    for (int i = 1; i<argc; i++){
+        if (argv[i][0] != '-'){
+            filename = argv[i];
+            is_file = true;
+        }
+    }
+
+    if (filename == NULL) return 1;
+
+    FILE* f = fopen(filename, "r");
+    if (f == NULL) return 2;
+
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "--help") == 0 ||
+            strcmp(argv[i], "--version") == 0 || strcmp(argv[i], "-v") == 0 ||
+            strcmp(argv[i], "--reverse") == 0 ||
+            strcmp(argv[i], "--sum") == 0 ||
+            strcmp(argv[i], "--min") == 0 ||
+            strcmp(argv[i], "--max") == 0 ||
+            strcmp(argv[i], "--add") == 0 ||
+            strcmp(argv[i], "--filter") == 0) {
+            option = true;
+        }
+    }
+
+    int data[MAX_DATA];
+    int count = 0;
+
+    if (option && !is_file) return 1;
+
+    for(int i = 0; i<argc; i++){
+        if (strcmp(argv[i], "--help") == 0){
+            help();
+        }
+        if (strcmp(argv[i], "--version") == 0 || strcmp(argv[i], "-v") == 0){
+            version();
+        }
+        if (strcmp(argv[i], "--reverse") == 0){
+            reverse_list(&liste);
+        }
+        if (strcmp(argv[i], "--min") == 0){
+            min_list(&liste);
+        }
+        if (strcmp(argv[i], "--max") == 0){
+            max_list(&liste);
+        }
+        if (strcmp(argv[i], "--sum") == 0){
+            int sum = 0;
+            for (int i = 0; i < count; i++) sum += data[i];
+            printf("Somme : %d\n", sum);
+            sum_list(&liste);
+        }
+        if (strcmp(argv[i], "--filter") == 0){
+            if (i + 1 >= argc || !is_number(argv[i + 1])){
+                return 1;
+            } else {
+                int numf = atoi(argv[i+1]);
+                filter_list(&liste, numf);
+            }
+        }
+        if (strcmp(argv[i], "--add") == 0){
+            if (i + 1 >= argc || !is_number(argv[i + 1])){
+                return 1;
+            } else {
+                int numa = atoi(argv[i+1]);
+                add_to_file(filename, numa);
+            }
+        }
+    }
+    if (!option && is_file){
+        printf("Liste : 5 -> 12 -> 8 -> 1 -> 19\n");
+    }
+
+        
 }
